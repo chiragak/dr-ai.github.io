fa973c102240bbddc0aaf70be8850d85cfa384ab
Chirag A K <70628014+chiragak@users.noreply.github.com>
2023-08-25 19:03:31 +0530

    modified script.js in .

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ const loader = document.getElementById('loader');
 
 // Construct the Vertex AI endpoint URL
 const endpointId = "5043330094236434432"; // Update with your actual endpoint ID
-const region = "us-central1";
 const projectId = "714552415904"; // Update with your actual project ID
-const endpointUrl = `https://${region}-${projectId}.cloudai.endpoints.${projectId}.cloud.goog/${endpointId}`;
 
 imageUpload.addEventListener('change', async (e) => {
   const imageFile = e.target.files[0];
@@ -16,19 +14,31 @@ imageUpload.addEventListener('change', async (e) => {
     try {
       loader.style.display = 'block';
 
-      const formData = new FormData();
-      formData.append('image', imageFile);
+      const reader = new FileReader();
+      reader.onload = async (event) => {
+        const imageBytes = event.target.result.split(',')[1]; // Extract base64-encoded image data
 
-      const response = await fetch(endpointUrl, {
-        method: 'POST',
-        body: formData,
-      });
+        const requestData = {
+          instances: [{ content: imageBytes }],
+        };
 
-      const predictionData = await response.json();
-      const predictedClass = predictionData.predictedClass;
+        const response = await fetch(`https://us-central1-aiplatform.googleapis.com/v1/projects/${projectId}/locations/us-central1/endpoints/${endpointId}:predict`, {
+          method: 'POST',
+          headers: {
+            'Authorization': `Bearer ${gapi.auth.getToken().access_token}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(requestData),
+        });
 
-      result.textContent = `Predicted class: ${predictedClass}`;
-      result.style.color = 'green';
+        const predictionData = await response.json();
+        const predictedClass = predictionData.predictions[0].predictedClass; // Update based on actual response structure
+
+        result.textContent = `Predicted class: ${predictedClass}`;
+        result.style.color = 'green';
+      };
+
+      reader.readAsDataURL(imageFile);
     } catch (error) {
       console.error('Error making prediction:', error);
       result.textContent = 'Error making prediction';
