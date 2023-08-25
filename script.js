20ea2455e9953978239356ca7fd5851663a534c1
Chirag A K <70628014+chiragak@users.noreply.github.com>
2023-08-25 21:03:57 +0530

    modified script.js in .

--- script.js
+++ script.js
@@ -1,66 +1,27 @@
+// Get your API key from Vertex AI API & Services dashboard 
+const API_KEY = 'AIzaSyB0zoeUGQ2ZYwfld09AeC3JF-0p9PAzSAc'; 
 
-const endpoint = "https://us-central1-PROJECT_ID.cloud-aiplatform.googleapis.com/v1/projects/PROJECT_ID/locations/us-central1/endpoints/ENDPOINT_ID:predict";
+// Construct the endpoint URL
+const MODEL_ENDPOINT = "https://us-central1-PROJECT_ID.cloud-aiplatform.googleapis.com/v1/projects/PROJECT_ID/locations/us-central1/endpoints/ENDPOINT_ID:predict";
 
-// Load image
-function loadImage(event) {
-  const image = document.getElementById('selected-image');
-  image.src = URL.createObjectURL(event.target.files[0]);
-}
+// Authentication headers 
+const headers = {
+  'Authorization': `Bearer ${API_KEY}`,
+  'Content-Type': 'application/json'
+};
 
-// Predict image
-async function predictImage() {
+// Image data to send
+const imageData = {
+  b64: '<Base64 encoded image>' 
+};
 
-  const loader = document.getElementById('loader');
-  loader.style.display = 'block';
+// Make prediction request
+const response = await fetch(MODEL_ENDPOINT, {
+  method: 'POST',
+  headers: headers,
+  body: JSON.stringify({instances: [imageData]})
+});
 
-  try {
-    
-    // Get image
-    const image = document.getElementById('image-upload').files[0];
-
-    // Classify image
-    const response = await fetch(endpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        instances: [{
-          image: {
-            imageBytes: image 
-          }
-        }]
-      })
-    });
-
-    const data = await response.json();
-    const predictedClass = data.predictions[0].displayNames[0];
-
-    // Display result
-    displayResult(predictedClass);
-
-  } catch (error) {
-    displayError(error); 
-  }
-
-  loader.style.display = 'none';
-
-}
-
-// Display result 
-function displayResult(className) {
-  
-  const result = document.getElementById('result');
-  result.innerHTML = `Predicted: <strong>${className}</strong>`;
-  result.style.color = 'green';
-
-}
-
-// Display error
-function displayError(error) {
-
-  const result = document.getElementById('result');
-  result.innerHTML = 'Error loading prediction';
-  result.style.color = 'red';
-
-}
+// Parse response
+const data = await response.json();
+const prediction = data.predictions[0];
