afef98225ae4cbdc0dcc99690cf4cc88cd0530c9
Chirag A K <70628014+chiragak@users.noreply.github.com>
2023-08-25 19:17:15 +0530

    modified index.html and script.js in .

--- script.js
+++ script.js
@@ -1,55 +1,24 @@
-const imageUpload = document.getElementById('image-upload');
-const submitBtn = document.getElementById('submit');
-const result = document.getElementById('result');
-const loader = document.getElementById('loader');
+const functions = require("firebase-functions");
+const fetch = require("node-fetch");
 
-// Construct the Vertex AI endpoint URL
-const endpointId = "5043330094236434432"; // Update with your actual endpoint ID
-const projectId = "714552415904"; // Update with your actual project ID
+// Replace with your actual Vertex AI model endpoint
+const MODEL_ENDPOINT = "https://us-central1-PROJECT_ID.cloud-aiplatform.googleapis.com/v1/projects/PROJECT_ID/locations/us-central1/endpoints/ENDPOINT_ID:predict";
 
-imageUpload.addEventListener('change', async (e) => {
-  const imageFile = e.target.files[0];
-
-  if (imageFile) {
+exports.classifyImage = functions.https.onRequest(async (req, res) => {
     try {
-      loader.style.display = 'block';
+        const image = req.body.image;
 
-      const reader = new FileReader();
-      reader.onload = async (event) => {
-        const imageBytes = event.target.result.split(',')[1]; // Extract base64-encoded image data
-
-        const requestData = {
-          instances: [{ content: imageBytes }],
-        };
-
-        const response = await fetch(`https://us-central1-aiplatform.googleapis.com/v1/projects/${projectId}/locations/us-central1/endpoints/${endpointId}:predict`, {
-          method: 'POST',
-          headers: {
-            'Authorization': `Bearer ${gapi.auth.getToken().access_token}`,
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(requestData),
+        // Forward the image to your Vertex AI model for classification
+        const response = await fetch(MODEL_ENDPOINT, {
+            method: "POST",
+            body: JSON.stringify({ image: image }),
+            headers: { "Content-Type": "application/json" },
         });
 
-        const predictionData = await response.json();
-        const predictedClass = predictionData.predictions[0].predictedClass; // Update based on actual response structure
-
-        result.textContent = `Predicted class: ${predictedClass}`;
-        result.style.color = 'green';
-      };
-
-      reader.readAsDataURL(imageFile);
+        const data = await response.json();
+        res.status(200).json(data);
     } catch (error) {
-      console.error('Error making prediction:', error);
-      result.textContent = 'Error making prediction';
-      result.style.color = 'red';
-    } finally {
-      loader.style.display = 'none';
+        console.error(error);
+        res.status(500).send("An error occurred.");
     }
-  }
-});
-
-submitBtn.addEventListener('click', () => {
-  // Trigger the file upload event when the "Predict" button is clicked
-  imageUpload.click();
 });
