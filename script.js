471e6d9a649ef1b99c2f29bf51c64b762640bc58
Chirag A K <70628014+chiragak@users.noreply.github.com>
2023-08-25 18:21:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,132 +1,59 @@
-let uploadButton = document.getElementById("upload-button");
-let chosenImage = document.getElementById("chosen-image");
-let fileName = document.getElementById("file-name");
-let container = document.querySelector(".container");
-let error = document.getElementById("error");
-let imageDisplay = document.getElementById("image-display");
+const imageUpload = document.getElementById('image-upload'); 
+const submitBtn = document.getElementById('submit');
+const result = document.getElementById('result');
+const loader = document.getElementById('loader');
 
-const fileHandler = (file, name, type) => {
-  if (type.split("/")[0] !== "image") {
-    //File Type Error
-    error.innerText = "Please upload an image file";
-    return false;
-  }
-  error.innerText = "";
-  let reader = new FileReader();
-  reader.readAsDataURL(file);
-  reader.onloadend = () => {
-    //image and file name
-    let imageContainer = document.createElement("figure");
-    let img = document.createElement("img");
-    img.src = reader.result;
-    imageContainer.appendChild(img);
-    imageContainer.innerHTML += `<figcaption>${name}</figcaption>`;
-    imageDisplay.appendChild(imageContainer);
-  };
-};
+// Vertex AI endpoint
+const endpoint = 'https://[ENDPOINT]';
 
-//Upload Button
-uploadButton.addEventListener("change", () => {
-  imageDisplay.innerHTML = "";
-  Array.from(uploadButton.files).forEach((file) => {
-    fileHandler(file, file.name, file.type);
-  });
+// Handle image upload
+imageUpload.addEventListener('change', (e) => {
+  // Upload code
 });
 
-container.addEventListener(
-  "dragenter",
-  (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    container.classList.add("active");
-  },
-  false
-);
+// Handle form submit
+submitBtn.addEventListener('click', async () => {
 
-container.addEventListener(
-  "dragleave",
-  (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    container.classList.remove("active");
-  },
-  false
-);
+  // Show loader
+  loader.style.display = 'block';
 
-container.addEventListener(
-  "dragover",
-  (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    container.classList.add("active");
-  },
-  false
-);
+  try {
 
-container.addEventListener(
-  "drop",
-  (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    container.classList.remove("active");
-    let draggedData = e.dataTransfer;
-    let files = draggedData.files;
-    imageDisplay.innerHTML = "";
-    Array.from(files).forEach((file) => {
-      fileHandler(file, file.name, file.type);
-    });
-  },
-  false
-);
+    // Get image file
+    const imageFile = imageUpload.files[0];
 
-window.onload = () => {
-  error.innerText = "";
-};
-
-let submitButton = document.getElementById("submit-button");
-
-submitButton.addEventListener("click", () => {
-});
-
-
-
-
-
-
-
-
-
-
-const submitButton = document.getElementById('submit');
-const resultElement = document.getElementById('result');
-
-submitButton.addEventListener('click', async () => {
-
-  const imageFile = document.getElementById('upload').files[0];
-
-  const endpoint = 'https://us-central1(lowa)-aiplatform.googleapis.com/diabetic_retinopathy';
-
-  const response = await fetch(endpoint, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      instances: [
-        {
-          image: {
-            imageBytes: imageFile // pass image as bytes
+    // Send POST request to Vertex AI
+    const response = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        instances: [
+          {
+            image: {
+              imageBytes: imageFile 
+            }
           }
-        }
-      ]
-    })
-  });
+        ]  
+      })
+    });
+
+    // Parse prediction response
+    const data = await response.json();
+    const predictedClass = data.predictions[0].displayNames[0];
+
+    // Display result  
+    result.textContent = `Predicted class: ${predictedClass}`;
+    result.style.color = 'green';
   
-  const prediction = await response.json();
+  } catch (error) {
+    // Handle error
+    result.textContent = 'Error making prediction';
+    result.style.color = 'red';
+  }
 
-  const predictedClass = prediction.predictions[0].displayNames[0];
-
-  resultElement.textContent = `Predicted class: ${predictedClass}`;
+  // Hide loader
+  loader.style.display = 'none';
 
 });
-
