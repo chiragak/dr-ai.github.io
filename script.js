5215a45fac4347f2e2de1db58167884cc1c90893
Chirag A K <70628014+chiragak@users.noreply.github.com>
2023-08-25 18:54:32 +0530

    modified script.js in .

--- script.js
+++ script.js
@@ -1,59 +1,44 @@
-const imageUpload = document.getElementById('image-upload'); 
+const imageUpload = document.getElementById('image-upload');
 const submitBtn = document.getElementById('submit');
 const result = document.getElementById('result');
 const loader = document.getElementById('loader');
 
-// Vertex AI endpoint
-const endpoint = 'https://[ENDPOINT]';
+// Construct the Vertex AI endpoint URL
+const endpointId = "5043330094236434432";
+const region = "us-central1";
+const projectId = "YOUR_PROJECT_ID";
+const endpointUrl = `https://${region}-${projectId}.cloudai.endpoints.${projectId}.cloud.goog/${endpointId}`;
 
-// Handle image upload
-imageUpload.addEventListener('change', (e) => {
-  // Upload code
+imageUpload.addEventListener('change', async (e) => {
+  const imageFile = e.target.files[0];
+
+  if (imageFile) {
+    try {
+      loader.style.display = 'block';
+
+      const formData = new FormData();
+      formData.append('image', imageFile);
+
+      const response = await fetch(endpointUrl, {
+        method: 'POST',
+        body: formData,
+      });
+
+      const predictionData = await response.json();
+      const predictedClass = predictionData.predictedClass;
+
+      result.textContent = `Predicted class: ${predictedClass}`;
+      result.style.color = 'green';
+    } catch (error) {
+      result.textContent = 'Error making prediction';
+      result.style.color = 'red';
+    } finally {
+      loader.style.display = 'none';
+    }
+  }
 });
 
-// Handle form submit
-submitBtn.addEventListener('click', async () => {
-
-  // Show loader
-  loader.style.display = 'block';
-
-  try {
-
-    // Get image file
-    const imageFile = imageUpload.files[0];
-
-    // Send POST request to Vertex AI
-    const response = await fetch(endpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        instances: [
-          {
-            image: {
-              imageBytes: imageFile 
-            }
-          }
-        ]  
-      })
-    });
-
-    // Parse prediction response
-    const data = await response.json();
-    const predictedClass = data.predictions[0].displayNames[0];
-
-    // Display result  
-    result.textContent = `Predicted class: ${predictedClass}`;
-    result.style.color = 'green';
-  
-  } catch (error) {
-    // Handle error
-    result.textContent = 'Error making prediction';
-    result.style.color = 'red';
-  }
-
-  // Hide loader
-  loader.style.display = 'none';
-
+submitBtn.addEventListener('click', () => {
+  // Trigger the file upload event when the "Predict" button is clicked
+  imageUpload.click();
 });
